611d72f4aba8f7719df47ee95a3b041b87ab8b7b
Nikitapss <144986879+Nikitapss@users.noreply.github.com>
2024-04-13 00:07:05 +0300

    123

--- Deflate.java
+++ Deflate.java
@@ -1,13 +1,57 @@
 import java.io.*;
 
 public class Deflate {
-    
+    public static void main(String[] args) {
+        try{
+            BetterInputStream bis = new BetterInputStream("test.txt");
+            System.out.println(bis.read());
+            bis.close();
+        }catch (Exception e){
+        }
+    }
 }
 
-class HuffmanMethod {
-
+class LZ77Code {
 }
-
-class LZ77Method {
+class HoffmanCode {
 }
+class HoffmanNode{
+    char a;
+    HoffmanNode left;
+    HoffmanNode right;
+    public HoffmanNode(char a){
+        this.a = a;
+        this.left = null;
+        this.right = null;
+    }
+}
+class BetterInputStream extends InputStream{
+    private FileInputStream fis;
+    public BetterInputStream(String path) throws FileNotFoundException{
+        this.fis = new FileInputStream(path);
+    }
+    public int read(){
+        int a = 0;
+        try{
+            a = fis.read();
+        } catch(Exception e){
 
+        }
+        return a;
+    }
+    public void close() throws IOException{
+        fis.close();
+    }
+}
+class BetterOutputStream extends OutputStream {
+    private FileOutputStream fos;
+    public BetterOutputStream(String path) throws FileNotFoundException{
+        this.fos = new FileOutputStream(path);
+    }
+    public void write(int i){
+
+    }
+    public void close() throws IOException{
+        fos.close();
+    }
+}
